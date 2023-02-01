9e8acc621cdb4dbb9a5180f275e35f5bb2c9a525
ltblem <jamsieh@icloud.com>
2023-02-01 20:49:20 +0000

    initial commit, everything i've done so far.

---
+++ cats.js
@@ -0,0 +1,3 @@
+document.write(
+    '<img src="https://theoldreader.com/kittens/' + window.innerWidth + '/'+ window.innerHeight + '"; width="' + window.innerWidth + '"; height="' + window.innerHeight + '">'
+    )
