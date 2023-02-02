a889ce6acfe08dc8eb888e747dec4952c778eb16
LetThereBeLemons <92221244+ltblem@users.noreply.github.com>
2023-02-02 09:58:05 +0000

    auto reload test

--- cats.js
+++ cats.js
@@ -1,3 +1,4 @@
 document.write(
     '<img src="https://theoldreader.com/kittens/' + window.innerWidth + '/'+ window.innerHeight + '"; width="' + window.innerWidth + '"; height="' + window.innerHeight + '">'
-)
+);
+setTimeout(() => { location.reload(); }, 1000);
