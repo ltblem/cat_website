11279f95149acab2ae03372541eeac52fb534195
ltblem <jamsieh@icloud.com>
2023-07-16 14:07:17 +0100

    this is as much as i can do for now

--- cats.js
+++ cats.js
@@ -16,9 +16,11 @@ function ajax_get(url, callback) {
     xmlhttp.open("GET", url, true);
     xmlhttp.send();
 }
-  
-ajax_get('https://api.thecatapi.com/v1/images/search?size=full', function(data) {
+
+
+ajax_get('https://api.thecatapi.com/v1/images/search?api_key=live_W2PxBHqGJN8L6WZ2NNOcGPvflkQ6OTOiTTYTY0X4KD41Zj3r0PRFyPdyr9P4RzE5', function(data) {
     var html = '<img src="' + data[0]["url"] + '">';
     document.getElementById("image").innerHTML = html;
 });
-  
+
+//TODO: Make the returned image the correct aspect ratio
