593b9203e848aad028e8231bc2e32086a0250d99
ltblem <jamsieh@icloud.com>
2023-07-16 13:12:14 +0100

    ctrl+c, ctrl+v

--- cats.js
+++ cats.js
@@ -1,7 +1,25 @@
-document.write(
-    '<img src="https://theoldreader.com/kittens/' + window.innerWidth + '/'+ window.innerHeight + '"; width="' + window.innerWidth + '"; height="' + window.innerHeight + '">'
-);
-document.onkeydown = function() {
-    location.reload()
+function ajax_get(url, callback) {
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            console.log('responseText:' + xmlhttp.responseText);
+            try {
+                var data = JSON.parse(xmlhttp.responseText);
+            } catch (err) {
+                console.log(err.message + " in " + xmlhttp.responseText);
+                return;
+            }
+            callback(data);
+        }
+    };
+  
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();
 }
-//TODO: Add notice to the webpage along the lines of "press any key to reload"
+  
+ajax_get('https://api.thecatapi.com/v1/images/search?size=' + window.innerWidth + 'x' + window.innerHeight, function(data) {
+    var html = '<img src="' + data[0]["url"] + '">';
+    document.getElementById("image").innerHTML = html;
+});
+
+// TODO: fix image width and height
